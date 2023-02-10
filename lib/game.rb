e23c07fcf2c848e924ac90de28542e7b3ac785d9
Morfenza <ianlourenco@protonmail.com>
2023-02-10 19:15:37 -0300

    Implement basic game loop (no messages, only logic)

--- lib/game.rb
+++ lib/game.rb
@@ -1,27 +1,49 @@
 # frozen_string_literal: true
 
+require_relative 'modules/check'
+require_relative 'modules/stickman'
+
 # Class defining the game
 class Game
-end
+  include Check
+  include HangedPerson
 
-def pick_word
-  word_file = File.read('google-10000-english.txt').split
-  word_array = word_file.select { |word| word.size.between?(5, 12) }
-  word_array.sample.split('')
-end
+  attr_reader :word, :spaces
 
-def create_spaces(word)
-  Array.new(word.size, ' ')
-end
+  def initialize
+    @word = pick_word
+    @spaces = create_spaces(@word)
+  end
 
-def insert_letter(letter, spaces, word)
-  return unless word.include?(letter)
+  def pick_word
+    word_file = File.read('google-10000-english.txt').split
+    word_array = word_file.select { |word| word.size.between?(5, 12) }
+    word_array.sample.split('')
+  end
 
-  spaces.map!.with_index do |space, index|
-    if word[index].eql?(letter)
-      letter
-    else
-      space
+  def create_spaces(word)
+    Array.new(word.size, ' ')
+  end
+
+  def insert_letter(letter)
+    @spaces.map!.with_index do |space, index|
+      if @word[index].eql?(letter)
+        letter
+      else
+        space
+      end
+    end
+  end
+
+  def play_round
+    letter = gets.chomp
+    insert_letter(letter) if letter?(letter, @word)
+  end
+
+  def play_match
+    until won?(@spaces, @word)
+      p @spaces
+      play_round
     end
   end
 end
