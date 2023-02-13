1e0831c5383f9bed7b3bfe6012ac3b64328d8871
Morfenza <ianlourenco@protonmail.com>
2023-02-13 15:26:18 -0300

    Create option checkers

--- lib/modules/check.rb
+++ lib/modules/check.rb
@@ -9,4 +9,41 @@ module Check
   def letter?(letter, word)
     word.include?(letter)
   end
+
+  def again?
+    check = gets.chomp
+    return true if check == 'y'
+
+    false
+  end
+
+  def check_option
+    loop do
+      option = gets.chomp
+      return option.to_i if %w[1 2].include?(option)
+
+      puts 'Please, choose a valid option'
+    end
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  def check_letter(guesses)
+    loop do
+      letter = gets.chomp.downcase
+      if letter.size == 1 && letter.match?(/[a-zA-Z]/)
+        if guesses.include?(letter)
+          puts 'Please, write a letter that has not been written before'
+          next
+        end 
+        return letter
+
+      elsif letter == 'save'
+        return 2
+
+      end
+
+      puts 'Please, write a valid option'
+    end
+  end
+  # rubocop:enable Metrics/MethodLength
 end
